a07160be06da246c10a1a5f942bca4dd5157aaf2
DirkGaston <dirkgaston87@gmail.com>
2022-09-14 20:46:38 -0300

    fix: relative path issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <div className="homeContainer">
       <h1>Diario de un Consumidor de Cultura Pop</h1>
-      <img src="/src/assets/DirkOscar.jpg" alt="DirkOscar" />
+      <img src="./images/DirkOscar.jpg" alt="DirkOscar" />
       <h2>Ganador de un Oscar</h2>
     </div>
   );
